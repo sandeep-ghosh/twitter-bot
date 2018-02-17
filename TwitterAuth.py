d14e8340a44f96820a7e051800dcd678b6d88159
Sandeep <sandeep.extreme@gmail.com>
2018-02-17 17:39:33 -0500

    commit

---
+++ TwitterAuth.py
@@ -0,0 +1,8 @@
+'''
+Put these values from twitter developer API.
+More information provided here https://apps.twitter.com/
+'''
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
